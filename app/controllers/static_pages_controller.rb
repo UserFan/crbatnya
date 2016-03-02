2d8ef46a6e0fa3a1fe4c33951e57261188091bd9
Fanis <habfanis@mail.ru>
2016-03-02 12:22:47 +0300

    020316

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,26 @@ class StaticPagesController < ApplicationController
   	add_breadcrumb "Главная", root_path
     add_breadcrumb "О больнице", about_path
   end
+
+  def list_drugs
+  	add_breadcrumb "Главная", root_path
+  	add_breadcrumb "Перечень жизненно необходимых и важнейших лекарственных препаратов", list_drugs_path
+  end
+
+  def preferential_drugs
+    add_breadcrumb "Главная", root_path
+  	add_breadcrumb "Льготное обеспечение лекарственными препаратами", preferential_drugs_path
+  end
+  
+  def guarantee_program
+    add_breadcrumb "Главная", root_path
+  	add_breadcrumb "Программа государственных гарантий", guarantee_program_path  	
+  end
+  
+  def insurance_company
+    add_breadcrumb "Главная", root_path
+  	add_breadcrumb "Адреса и контакты страховых компаний", insurance_company_path
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root  'static_pages#home'
   
   get '/about', to: 'static_pages#about'
+  get '/list_drugs', to: 'static_pages#list_drugs'
+  get '/preferential_drugs', to: 'static_pages#preferential_drugs'
+  get '/guarantee_program', to: 'static_pages#guarantee_program'
+  get '/insurance_company', to: 'static_pages#insurance_company'
 end
