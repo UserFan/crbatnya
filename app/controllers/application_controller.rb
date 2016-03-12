6fbe557f84411532223fe60c45690a0b9b06ea5c
Azat Gataoulline <gataniel@gmail.com>
2016-03-12 22:30:34 +0300

    ASD*J

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   def resource_name
     :user
   end
@@ -16,7 +17,6 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :resource, :resource_name, :devise_mapping
-
 
   protected
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     add_breadcrumb "Главная", root_path
   end
-  
+
   def about
   	add_breadcrumb "Главная", root_path
     add_breadcrumb "О больнице", about_path
@@ -17,16 +17,14 @@ class StaticPagesController < ApplicationController
     add_breadcrumb "Главная", root_path
   	add_breadcrumb "Льготное обеспечение лекарственными препаратами", preferential_drugs_path
   end
-  
+
   def guarantee_program
     add_breadcrumb "Главная", root_path
-  	add_breadcrumb "Программа государственных гарантий", guarantee_program_path  	
+  	add_breadcrumb "Программа государственных гарантий", guarantee_program_path
   end
-  
+
   def insurance_company
     add_breadcrumb "Главная", root_path
   	add_breadcrumb "Адреса и контакты страховых компаний", insurance_company_path
   end
-
-
 end
